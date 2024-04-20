2a99a2d741893512a4b35507fcb1f50f95d1bcf0
RasulXaydarov <rasul_457@proton.me>
2024-04-20 15:07:06 +0500

    sound add

---
+++ script/soundEffect.js
@@ -0,0 +1,8 @@
+export let siteJumpSound;
+export let siteDeathSound;
+export let sitePointsound;
+
+
+siteJumpSound = new Audio("./assets/sound/jump.wav");
+siteDeathSound = new Audio("./assets/sound/die.wav");
+sitePointsound = new Audio("./assets/sound/point.wav");
