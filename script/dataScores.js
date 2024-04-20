a374b0fd410f12c27b824e528e364af25cdda2e9
RasulXaydarov <rasul_457@proton.me>
2024-04-20 13:52:39 +0500

    Corrected the variable in dinosaurData

--- script/dataScores.js
+++ script/dataScores.js
@@ -1,9 +1,10 @@
 let stage = "";
 const dinosaurData = {
     scores: {
-        stageOneScore: 600,
-        stageTwoScore: 1200,
-        stageThreeScore: 1800,
-        stageFourScour: 2400,
+        stageOneScore: 0,
+        stageOneScore: 1200,
+        stageTwoScore: 1800,
+        stageThreeScore: 2400,
+        stageFourScore: 3200,
     }
 };
