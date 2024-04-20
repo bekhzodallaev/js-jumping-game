f49c54de388de93dcd7c5a13c97316761977f82e
RasulXaydarov <rasul_457@proton.me>
2024-04-20 13:41:45 +0500

    Add scores for certain stages

---
+++ script/dataScores.js
@@ -0,0 +1,9 @@
+let stage = "";
+const dinosaurData = {
+    scores: {
+        stageOneScore: 600,
+        stageTwoScore: 1200,
+        stageThreeScore: 1800,
+        stageFourScour: 2400,
+    }
+};
