cd544fc874ff0f18460bde76de479e643dd3a5de
bekhzod <icebreaker0210@mail.ru>
2024-04-21 18:50:54 +0200

    Remove some unnecessary comments

--- script/dinosaur-movement.js
+++ script/dinosaur-movement.js
@@ -1,15 +1,6 @@
 import { dinoDetails } from "./main.js";
 import { context } from "./main.js";
 
-console.log(dinoImageRunOne);
-console.log(dinosaurRunTwo);
-
-
-
-
-// We should add two canvas images running dinasour
-// we should import import two image from the main
-// The function should toggle the images and run the dinasour
 let dinoImageRunOne = new Image();
 let dinoImageRunTwo = new Image();
 
@@ -20,24 +11,23 @@ dinoImageRunTwo.src = "./assets/dino-run-1.png";
 
 dinoImageRunOne.drawImage();
 function canvasDrawImage() {
-    context.drawImage(dinoImageRunOne, dinoDetails.x, dinoDetails.y, dinoDetails.width, dinoDetails.height);
+  context.drawImage(
+    dinoImageRunOne,
+    dinoDetails.x,
+    dinoDetails.y,
+    dinoDetails.width,
+    dinoDetails.height
+  );
 }
 dinoImageRunOne.addEventListener("load", { canvasDrawImage });
 dinoImageRunTwo.drawImage();
 function canvasDrawImage() {
-    context.drawImage(dinoImageRunOne, dinoDetails.x, dinoDetails.y, dinoDetails.width, dinoDetails.height);
+  context.drawImage(
+    dinoImageRunOne,
+    dinoDetails.x,
+    dinoDetails.y,
+    dinoDetails.width,
+    dinoDetails.height
+  );
 }
 dinoImageRunOne.addEventListener("load", { canvasDrawImage });
-// setAttribute("class", "hidden")
-
-
-// Move long time
-// Countiue running in the screen
-
-
-
-function runDinasour() {
-
-
-
-}
