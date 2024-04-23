209ecaa6856033a4cf95c65f72f93077091bc58e
bekhzod <icebreaker0210@mail.ru>
2024-04-23 23:29:33 +0200

    Remove spacing between dino and cactus

--- script/main.js
+++ script/main.js
@@ -209,11 +209,13 @@ function placeCactus() {
 
 // Function to check for collision between two objects
 function checkCollision(a, b) {
+  const buffer = 20;
+
   return (
-    a.x < b.x + b.width &&
-    a.x + a.width > b.x &&
-    a.y < b.y + b.height &&
-    a.y + a.height > b.y
+    a.x + a.width - buffer > b.x &&
+    a.x + buffer < b.x + b.width &&
+    a.y + a.height - buffer > b.y &&
+    a.y + buffer < b.y + b.height
   );
 }
 
